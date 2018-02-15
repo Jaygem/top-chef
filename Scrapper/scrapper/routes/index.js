29a4672006522328cd8ca9158101089768362cc9
Jaygem <youness.kafia@devinci.fr>
2018-02-15 22:34:35 +0100

    Trying to make it work with node js express app

--- Scrapper/scrapper/routes/index.js
+++ Scrapper/scrapper/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var tools = require('/public/javascripts/scrapMichelin.js')
 
 /* GET home page. */
 router.get('/', function(req, res){
   res.render('index', {
-    title: 'Home'
+    title: 'WebScrapper',
+    path : '/index.jade'
   });
 });
+
 
 module.exports = router;
