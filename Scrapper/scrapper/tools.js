c4c5bc7cf921636ef883aafa62538c4d3f83d921
Jaygem <youness.kafia@devinci.fr>
2018-03-04 00:29:51 +0100

    Just the scrapper "finished" in tool.js

--- Scrapper/scrapper/tools.js
+++ Scrapper/scrapper/tools.js
@@ -9,115 +9,96 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var getJSON = require('get-json')
+var each = require('async-each');
 
-// The URL we will scrape from - in our example Anchorman 2.
+
+// The URL we will scrape from 
 
 url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/';
 url2 = "https://restaurant.michelin.fr/search-restaurants?localisation=1424&cooking_type=&gm_selection=&stars=1%7C%7C2%7C%7C3&bib_gourmand=&piecette=&michelin_plate=&services=&ambiance=&booking_activated=&min_price=&max_price=&number_of_offers=&prev_localisation=1424&latitude=&longitude=&bbox_ne_lat=&bbox_ne_lon=&bbox_sw_lat=&bbox_sw_lon=&page_number=0&op=Rechercher&js=true"
-url3 = "https://www.lafourchette.com/recherche/?idCity=415144&idProductLine=4792008e-cd3f-4616-b300-693fcbd13698#filters%5BPROMOTION%5D%5B50_PERCENT%5D=on&filters%5BPROMOTION%5D%5B40_PERCENT%5D=on&filters%5BPROMOTION%5D%5B30_PERCENT%5D=on&filters%5BPROMOTION%5D%5B20_PERCENT%5D=on&filters%5BPROMOTION%5D%5BPRESTIGE_MENU%5D=on&filters%5BPROMOTION%5D%5BOTHER%5D=on&filters%5BPRODUCT_LINE%5D%5B4792008e-cd3f-4616-b300-693fcbd13698%5D=on&page="
-// The structure of our request call
-// The first parameter is our URL
-// The callback function takes 3 parameters, an error, response status code and the html
+url3 = "https://www.lafourchette.com/restaurant+paris?page=2&sort=POPULARITY_DESC&filters[PROMOTION][50_PERCENT]=on&filters[PROMOTION][40_PERCENT]=on&filters[PROMOTION][30_PERCENT]=on&filters[PROMOTION][20_PERCENT]=on&filters[PROMOTION][PRESTIGE_MENU]=on&filters[PROMOTION][OTHER]=on&filters[PRODUCT_LINE][4792008e-cd3f-4616-b300-693fcbd13698]=on&page="
 
+
+
+
+
+var allJson = [];
+var OnlyTheOnes = [];
 var arrayOfRestaurants = [];
-var arrayOfPromotions =[];
 var thisUrl;
 var j = 0;
 var json = {};
-var json2 = {};
 
 var customHeaderRequest = request.defaults({headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
 })
-/*
-for(i=1;i<35;i++)
+var file1 = fs.createWriteStream('Michelin.json');
+file1.on('error', function(err) { /* error handling */ });
+var arrayOfRequest = []
+console.log('Searching for starred restaurants')
+for(let i=1;i<35;i++)
 {
-    thisUrl = url+'page-'+j;
-    j++;
-    customHeaderRequest.get(thisUrl,function(error, response, html){
-        
-        
-            var $ = cheerio.load(html);
-
-            $('.poi_card-display-title').filter(function(){
-                var data = $(this);
-                json.title = data.first().text();
-            })
-            
-            $('.mtpb2c-offers').filter(function(){
-                var data = $(this);
-
-           // Once we have our title, we'll store it to the our json object.
-
-                json.offers =  data.text();
-            })
-            $('.poi_card-display-price').filter(function(){
-                var data = $(this);
-
-                priceRange = data.text();
-
-           // Once we have our title, we'll store it to the our json object.
-
-
-                json.price = priceRange;
-                arrayOfRestaurants.push(json);
-            })
-        //console.log(json);
-
-        
-
-    })
+   
     
-    }
-*/
-j = 1;
-
-//var html = '<ul class="list-unstyled"></ul>';
-
-for(i=1;i<79;i++)
-{
-    thisUrl2 = url3+j;
-    j++;
-    
-    customHeaderRequest.get(thisUrl2, function(error, response, html){
-        
-            
-        
+    arrayOfRequest.push(customHeaderRequest.get(url+'page-'+i,function(error, response, html){
+        var dp = "ds-1col node node--poi view-mode-poi_card node--poi-card node--poi--poi-card clearfix gtm-jsevent-processed" 
             var $ = cheerio.load(html);
         
-            //console.log($('.resultItem-address').text());
-            var allData = $('.resultItem').children().text().split('*');
-            allData.forEach(function(elem){
-                elem.split('\n');
-                console.log(elem+'\n\n\n');
-            })
-            /*
-            
-            $('.resultItem-name').filter(function(){
-                var data = $(this);
-                json2.Name = data.text();
-            })
-        
-        
-            $('.resultItem-address').filter(function(){
-                var data = $(this);
-                json2.Address = data.text();
-            })
-            $('.resultItem-saleType resultItem-saleType--specialOffer').filter(function(){
-                var data = $(this);
-                json2.Offer =data.text();
-            })
-            $('.resultItem-averagePrice').filter(function(){
+        $('#panels-content-main-leftwrapper li').each(function(i, elm) {
+                if($(this).find('.poi_card-display-title').text() == '')
+                    return
+                json.name = $(this).find('.poi_card-display-title').text().replace(/(\r\n|\n|\r|          |")/gm,"");
+                json.averagePrice = $(this).find('.poi_card-display-price').text().replace(/(\r\n|\n|\r|         |")/gm,"");
+                json.Offer = $(this).find('.mtpb2c-offers').text().replace(/(\r\n|\n|\r|       |")/gm,"");
                 
-                var data = $(this);
+                file1.write('{"title" : "' + json.name + '" , "offer" : "' +json.Offer + '" , "price" : "'+json.averagePrice+ '"}\n');
+                arrayOfRestaurants.push(json);
+    
+                
+            });
 
-                json2.Price = data.text();
-                arrayOfPromotions.push(json2);
-            })
+    }))
+}
 
-        
-//console.log(json2);*/
+var file2 = fs.createWriteStream('Result.json');
+file2.on('error', function(err) { /* error handling */ });
+ var arrayOfFourchette = [];
+
+setTimeout(function(){ 
+    console.log('searching for Lafourchette equivalence')
+    
+    var cont = fs.readFileSync("Michelin.json","utf8");
+    var contents = cont.split('\n');
+    contents.pop();
+    contents.forEach(function(elem){
+        allJson.push( JSON.parse(elem));
     })
-     
-    }
-console.log(JSON.stringify(arrayOfPromotions));
-console.log(JSON.stringify(arrayOfRestaurants)); 
+    setTimeout(function(){ 
+        
+            
+            
+            each(allJson, function(elem) {
+                
+                getJSON('https://m.lafourchette.com/api/restaurant-prediction?name='+(elem).title, function(error, response){
+                
+                
+                if(response != undefined)
+                    {
+                        //console.log(response[0]);
+                        OnlyTheOnes.push(response[0]);
+                    }
+                })
+            }, function(error, contents) {
+            });
+        setTimeout(function(){
+            
+            each(OnlyTheOnes, function(elem){
+                if(elem != undefined)
+                    console.log(elem.name + ' has an offer and is starred !')
+            })
+        }, 10000)
+        
+    }, 3000); 
+}, 10000);
+
+
